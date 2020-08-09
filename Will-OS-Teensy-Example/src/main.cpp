23fb2648d076de5c46fafe74e71b0dae57f346b2
William Redenbaugh <wredenba@uci.edu>
2020-08-09 02:03:12 -0700

    updated subrepos, doing other stuff

--- Will-OS-Teensy-Example/src/main.cpp
+++ Will-OS-Teensy-Example/src/main.cpp
@@ -1,31 +1,36 @@
 #include <Arduino.h>
-#include "TeensyThreads.h"
+#include "OSThreadKernel.h"
+#include "OSMutexKernel.h"
 
 const int LED = 13;
 volatile int blinkcode = 0;
+MutexLock lock_test; 
+os_thread_id_t target_thread; 
 
 void blinkthread(void *parameters) {
-  os_thread_delay_ms(1);
   while(1) {    
+      lock_test.lockWaitIndefinite();
       digitalWrite(LED, HIGH);
       os_thread_delay_ms(100);
+
+      lock_test.unlock();
       digitalWrite(LED, LOW);
       os_thread_delay_ms(100);
   }
 }
 
-os_thread_id_t target_thread; 
-
 void setup() {
   delay(4000);
-  threads_init();
+
+  // Macro that initializes the primary thread. 
+  os_init();
   pinMode(LED, OUTPUT);
   target_thread = os_add_thread((thread_func_t)blinkthread, 0, -1, 0);
 }
 
 void loop() {
-  os_suspend_thread(target_thread);
   os_thread_delay_ms(2000);
-  os_resume_thread(target_thread);
+  lock_test.lockWaitIndefinite();
   os_thread_delay_ms(2000);
+  lock_test.unlock();
 }
