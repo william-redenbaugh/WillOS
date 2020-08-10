12b6dc8590b9b12d0e3a74eadca8fccfe0dd563d
William Redenbaugh <wredenba@uci.edu>
2020-08-09 18:10:51 -0700

    updated subrepos and testing

--- Will-OS-Teensy-Example/src/main.cpp
+++ Will-OS-Teensy-Example/src/main.cpp
@@ -10,11 +10,14 @@ os_thread_id_t target_thread;
 
 void blinkthread(void *parameters) {
   while(1) {    
-      lock_test.lockWaitIndefinite();
+      os_thread_waitbits_notimeout(THREAD_SIGNAL_0);
+      os_thread_clear(THREAD_SIGNAL_0);
+
+      //lock_test.lockWaitIndefinite();
       digitalWrite(LED, HIGH);
       os_thread_delay_ms(100);
 
-      lock_test.unlock();
+      //lock_test.unlock();
       digitalWrite(LED, LOW);
       os_thread_delay_ms(100);
   }
@@ -27,18 +30,18 @@ void printhello(void){
 void setup() {
   delay(4000);
   Serial.begin(115200);
+  
   // Macro that initializes the primary thread. 
   os_init();
-  // Startup our low priority work thread
-  setup_lwip_thread();
-  add_lwip_task(printhello, NULL, 1000);
+
   pinMode(LED, OUTPUT);
   target_thread = os_add_thread((thread_func_t)blinkthread, 0, -1, 0);
 }
 
 void loop() {
   os_thread_delay_ms(2000);
-  lock_test.lockWaitIndefinite();
+  //lock_test.lockWaitIndefinite();
+  os_signal_thread(THREAD_SIGNAL_0, target_thread);
   os_thread_delay_ms(2000);
-  lock_test.unlock();
+  //lock_test.unlock();
 }
