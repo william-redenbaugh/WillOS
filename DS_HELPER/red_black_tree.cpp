71211155233d1e2d1f93ddac0a4a574f2a4f434d
william-redenbaugh <wmredenbaugh@gmail.com>
2020-10-02 00:22:47 -0700

    updated red-black-tree

--- DS_HELPER/red_black_tree.cpp
+++ DS_HELPER/red_black_tree.cpp
@@ -1,1 +1,38 @@
 #include "red_black_tree.hpp"
+
+/*!
+*   @brief Helper function that handles most of the process of finding a given node based off a reference number
+*   @param RedBlackTreePointerNode* pointer to the node with the matching reference number 
+*   @param uint32_t reference_number number we are looking for 
+*/
+static RedBlackTreePointerNode *search_helper(RedBlackTreePointerNode *node, uint32_t reference_number){
+    // If we can't find anything, we return null
+    if(node == NULL)
+        return NULL; 
+
+    // If we find the node with our reference number 
+    if(node->reference_number == reference_number)
+        return node; 
+
+    // Checking if the left or right sides found the value. 
+    RedBlackTreePointerNode *left = search_helper(node->left, reference_number); 
+    RedBlackTreePointerNode *right = search_helper(node->right, reference_number); 
+
+    // If either left or right sides of the node found anything
+    // We return it!
+    if(left != NULL)
+        return left; 
+    else if(right != NULL)
+        return right;
+
+    // If nothing is found, we return null  
+    return NULL; 
+}
+
+/*!
+*   @returns Return as node based off the inputed reference number
+*   @param uint32_t reference_number we are searching for
+*/
+RedBlackTreePointerNode* RedBlackTreePointerModule::search(uint32_t reference_number){
+    return search_helper(this->head, reference_number);
+}
--- DS_HELPER/red_black_tree.hpp
+++ DS_HELPER/red_black_tree.hpp
@@ -7,10 +7,71 @@
 
 #include <Arduino.h>
 
+/*!
+*   @brief Node Pointer node module that helps us manage the Red black tree. 
+*/
+struct RedBlackTreePointerNode{
+    /*!
+    *   @brief Node reference number that we calculate our sorting arithmatic on 
+    */
+    uint32_t reference_number; 
+
+    /*!
+    *   @brief General purpose pointer that let's us fetch anything around the systm 
+    */
+    void *ptr;
+
+    /*!
+    *   @brief Flag that determines whether or not node is red or black
+    *   @note  True: Red. False: Black,
+    */
+    bool red_black = false; 
+
+    /*!
+    *   @brief Left and right pointer nodes for the tree. 
+    */
+    struct RedBlackTreePointerNode *left;   
+    struct RedBlackTreePointerNode *right; 
+};
+
+/*!
+*   @brief Module that lets us generate a red-black tree. 
+*/
 class RedBlackTreePointerModule{
 public: 
+    /*!
+    *   @returns Return as node based off the inputed reference number
+    *   @param uint32_t reference_number we are searching for
+    */
+    RedBlackTreePointerNode* search(uint32_t reference_number); 
+
+    /*!
+    *   @returns Find the largest known value in the tree. 
+    */
+    RedBlackTreePointerNode* max(void); 
+
+    /*!
+    *   @returns the smallest known value in the tree
+    */
+    RedBlackTreePointerNode* min(void); 
+
+    /*!
+    *   @brief Inserts a node into the tree with a given reference number and value
+    *   @param uint32_t reference number
+    *   @param void* general purpose pointer. 
+    */  
+    void insert(void *ptr, uint32_t reference_number); 
+
+    /*!
+    *   @brief deletes a node with a given reference number
+    *   @param uint32_t reference number
+    *   @returns bool whether or not we could remove node. 
+    */
+    bool remove(uint32_t reference_number); 
 
 private: 
+
+    RedBlackTreePointerNode *head; 
 
 };
 
