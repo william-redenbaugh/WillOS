24969b8cd1cd0fee9ca0b4a468f9a681f9bf4824
William Redenbaugh <wmredenbaugh@gmail.com>
2023-03-27 17:05:59 -0700

    completed OS queue

--- CSAL/os_queue.cpp
+++ CSAL/os_queue.cpp
@@ -1,5 +1,18 @@
 #include "os_queue.h"
 #include "os_error.h"
+
+inline void push_element(os_queue_t *queue, void *data){
+    queue->data[queue->head] = data;
+    queue->head++;
+    queue->current_num_elements++;
+}
+
+inline void* pop_element(os_queue_t *queue){
+    void *data = queue->data[queue->tail] = data;
+    queue->tail++;
+    queue->current_num_elements--;
+}
+
 int os_queue_init(os_queue_t *queue, int max_elements){
     if(queue == NULL){
         return OS_RET_INVALID_PARAM;
@@ -28,24 +41,51 @@ int os_queue_push_timeout(os_queue_t *queue, void *data, int timeout_ms){
         ret = OS_RET_LIST_EMPTY;
         goto queue_end;
     }
-
-    queue->data[queue->head];
-    queue->head++;
-    queue->current_num_elements++;
     
+    push_element(queue, data);
+
     queue_end: 
     os_mut_exit(&queue->queue_lock);
     return ret;
 }
 
 int os_queue_push_indefinite(os_queue_t *queue, void *data, int timeout_ms){
+    int ret = OS_RET_OK;
 
+    os_mut_entry_wait_indefinite(&queue->queue_lock);
+    
+    if(queue->current_num_elements == queue->max_num_elemnts){
+        ret = OS_RET_LIST_EMPTY;
+        goto queue_end;
+    }
+    
+    push_element(queue, data);
+
+    queue_end: 
+    os_mut_exit(&queue->queue_lock);
 }
 
 void* os_queue_pop_timeout(os_queue_t *queue, int timeout_ms){
+    void *data = NULL;
 
+    os_mut_entry(&queue->queue_lock, timeout_ms);
+
+    if(queue->current_num_elements !=  0){        
+        data = pop_element(queue);
+    }
+    os_mut_exit(&queue->queue_lock);
+    return data;
 }
 
 void *os_queue_pop_indefinite(os_queue_t *queue){
+    void *data = NULL;
+    
+    os_mut_entry_wait_indefinite(&queue->queue_lock);
 
+    if(queue->current_num_elements !=  0){        
+        data = pop_element(queue);
+    }
+
+    os_mut_exit(&queue->queue_lock);
+    return data;
 }
