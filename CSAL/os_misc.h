7503af5bb183540c9fac4d02db3b5be87c4d824a
William Redenbaugh <wmredenbaugh@gmail.com>
2023-09-02 22:33:33 -0700

    burn with fire

--- CSAL/os_misc.h
+++
@@ -1,8 +0,0 @@
-#ifndef _OS_MISC_H
-#define _OS_MISC_H
-
-//#include "Arduino.h"
-
-#define println(e) (void)e
-
-#endif
--- CSAL/os_thread.h
+++
@@ -1,6 +0,0 @@
-#ifndef _OS_THREAD_H
-#define _OS_THREAD_H
-
-#include "../OS/OSThreadKernel.h"
-
-#endif
